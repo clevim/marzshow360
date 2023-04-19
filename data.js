f7801018d29b6f6799bed99a41ecb0dd2b43fd97
Clever Guimarães <clever.filho@gmail.com>
2023-04-19 15:24:36 -0300

    TEste

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-timothy-oldfield-luufnhochru-unsplash",
+      "name": "timothy-oldfield-luufnHoChRU-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.8714170038088023,
+        "pitch": 0.47122822524722174,
+        "fov": 1.3848081938437478
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6086861869397486,
+          "pitch": 0.039349351220423046,
+          "rotation": 3.141592653589793,
+          "target": "1-bryan-goff-iuyhxaia8ea-unsplash"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bryan-goff-iuyhxaia8ea-unsplash",
+      "name": "bryan-goff-IuyhXAia8EA-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.07039108513079206,
+        "pitch": -0.6385878847183317,
+        "fov": 1.3848081938437478
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3583752836506378,
+          "pitch": -0.1609462434426181,
+          "rotation": 0,
+          "target": "0-timothy-oldfield-luufnhochru-unsplash"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "TESTEPAGE",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
